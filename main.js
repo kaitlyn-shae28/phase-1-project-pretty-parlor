c01d3f92e8dee95d64be6b5f92ab6e47a5cda380
Kaitlyn <kaitlyn.shae28@gmail.com>
2023-12-26 22:28:24 -0500

    Began to add buttons to cards, started the process of adding an alert to the buttons.

---
+++ main.js
@@ -0,0 +1,9 @@
+const bookButtons = document.getElementById('appointment-button')
+
+// bookButtons.forEach(bookButton => {
+//     bookButtons.addEventListener('click', () => {
+//         alert('Appointment Booked!');
+//     })
+// })
+
+// NEED TO MAKE ALL BUTTONS POP UP!!!!
