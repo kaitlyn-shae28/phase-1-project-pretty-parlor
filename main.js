c76f14b8e028af1ac2eb75a57e16bb1de536cc10
Kaitlyn <kaitlyn.shae28@gmail.com>
2023-12-28 00:34:30 -0500

    Started created the filter for the differenet types of appointments in html and java.

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 const bookButtons = document.getElementById('appointment-button')
 
+document.addEventListener('DOMContentLoaded', () => {
+    const typeDropdown = document.querySelector('#select-type');
+    typeDropdown.addEventListener('change', (event) => {
+    const selectedType = event.target.value;
+    filterTypes(selectedType);
+  });
+});
+
+function filterTypes(selectedType) {
+    const appointmentTypes = document.querySelector('.card');
+      
+    appointmentTypes.forEach((appointmentType) => {
+        var getIds = document.getElementById(selectedType.value)
+        if (appointmentType.id === getIds) {
+           appointmentTypes.style.display = 'block';
+        } else {
+              appointmentTypes.style.display = 'none';
+        }
+    });
+}
+
 // bookButtons.forEach(bookButton => {
 //     bookButtons.addEventListener('click', () => {
 //         alert('Appointment Booked!');
