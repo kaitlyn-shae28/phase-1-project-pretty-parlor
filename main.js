08e6372ec0cb0b83a6709a3966826b1f14d5c933
Kaitlyn <kaitlyn.shae28@gmail.com>
2024-01-08 01:01:12 -0500

    began restructuring filter

--- main.js
+++ main.js
@@ -9,17 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-function filterTypes(selectedType) {
-    const appointmentTypes = document.querySelector('.card');
-      
-    appointmentTypes.forEach((appointmentType) => {
-        var getIds = document.getElementById(selectedType.value)
-        if (appointmentType.id === getIds) {
-           appointmentTypes.style.display = 'block';
-        } else {
-              appointmentTypes.style.display = 'none';
-        }
+function filterCategory(selectedCategory) {
+    const a = arr.filter(el => el.Category === category);
+    console.log(a);
+
+    a.map(element => {
+        const div = document.createElement('div')
+        div.className = 'col-lg-4 col-md-4 col-sm-6 col-12 p-3'
+        div.innerHTML = `
+                    <div class="card" style="width: 18rem; height: 100% ">
+                            <div class="card-body">
+                          <h5 class="card-title">${element.Name}</h5>
+                           <p class="card-text">${element.Category}</p>
+                            </div>
+                    </div>
+                            `
+        row.appendChild(div)
     });
+
 }
 
 const buttons = document.querySelectorAll(".btn");
