4044a67357c03e7b3e6084797a28db8024fcf20c
Kaitlyn <kaitlyn.shae28@gmail.com>
2024-01-11 00:56:21 -0500

    reconfigured the DOM event listener with the fetch request, now working on the filter

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
-const bookButtons = document.getElementById('appointment-button')
-
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('db.json');
-        .then(response => response.json());
-    const typeDropdown = document.querySelector('#select-type');
-    typeDropdown.addEventListener('change', (event) => {
-    const category = event.target.value;
-    filterCategory(category);
-  });
+    fetch('http://localhost:3000/')
+        .then(response => response.text())
+        .then(data => {
+          const bookButtons = document.getElementById('appointment-button');
+          const typeDropdown = document.querySelector('#select-type');
+          typeDropdown.addEventListener('change', (event) => {
+          const category = event.target.value;
+          filterCategory(category, data.appointments);
+          });
+        })
+        .catch(error => {
+          console.error(error);
+        });
 });
 
-const row = document.querySelector('.row')
-function filterCategory(category) {
-    const a = arr.filter(el => el.category === category);
-    console.log(a);
+const row = document.querySelector('.row');
+function filterCategory(category, appointments) {
+    const filteredAppointments = appointments.filter(appointment => appointment.category === category);
+    console.log(filteredAppointments);
 
-    a.map(element => {
+    filteredAppointments.map(appointment => {
         const div = document.createElement('div');
         div.innerHTML = `
-                    <div class="card" id=${element.category} style="width: 18rem; height: 100% ">
-                        <img src= ${element.image}   
+                    <div class="card" id=${appointment.category} style="width: 18rem; height: 100% ">
+                        <img src= ${appointment.image}/>  
                         <div class="card-body">
-                            <h4><b>${element.name}</b></h4>
-                            <p>Starting at ${element.price}</p>
+                            <h4><b>${appointment.name}</b></h4>
+                            <p>Starting at ${appointment.price}</p>
                             <button class="btn">Book Appointment!</button>
                         </div>
                     </div>
                             `
          row.innerHTML = div;
-    });
-
+    })
 };
 
 const buttons = document.querySelectorAll(".btn");
@@ -38,4 +41,3 @@ buttons.forEach(function(button) {
     alert("Appointment Booked!");
   });
 });
-
